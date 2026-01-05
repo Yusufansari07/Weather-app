73ad31357b1133b7f30988efc4aa5dbab73b0c41
yusuf <yusufansarihouse@gmail.com>
2026-01-05 13:47:43 +0530

    EventListeners added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const apikey = "b27edd31a221c3e93ad38208c2a917c8";
 
+//DOM Elements
 const cityInput = document.getElementById("city");
 const searchBtn = documentGetElementById("search");
 
@@ -8,3 +9,20 @@ const tempE1 = documentGetElementById("temp");
 const conditionE1 = documentGetElementById("condition");
 const humidityE1 = documentGetElementById("humidity");
 const windE1 = documentGetElementById("wond");
+
+// Event Listeners
+searchBtn.addEventListener("click",handlesearch);
+cityInput.addEventListener("keydown",(e) => {
+    if(e.key === "Enter")handleSearch();
+});
+
+function handleSearch() {
+    const city = cityInput.Value.trim();
+
+    if(!city){
+        alert("Enter a city name:");
+        return;
+    }
+
+    fetchweather(city);
+}
