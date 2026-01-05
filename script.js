9df1cc140f0cb6b2d9e800ca772c83e403d2be65
yusuf <yusufansarihouse@gmail.com>
2026-01-05 13:37:00 +0530

    DOM elements added

---
+++ script.js
@@ -0,0 +1,10 @@
+const apikey = "b27edd31a221c3e93ad38208c2a917c8";
+
+const cityInput = document.getElementById("city");
+const searchBtn = documentGetElementById("search");
+
+const cityNameE1 = documentGetElementById("city-name");
+const tempE1 = documentGetElementById("temp");
+const conditionE1 = documentGetElementById("condition");
+const humidityE1 = documentGetElementById("humidity");
+const windE1 = documentGetElementById("wond");
